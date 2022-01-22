3a3329a771f99f6bebc20179653ccc4310ff3cab
fenil292 <65783083+fenil292@users.noreply.github.com>
2022-01-22 13:54:42 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
 });
 
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log('listening on *:3000');
 });
