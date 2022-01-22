32cc5705f8f47d813faa1a9a42d8c8d3e4d29530
fenil292 <65783083+fenil292@users.noreply.github.com>
2022-01-22 13:59:52 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const http = require('http');
 const server = http.createServer(app);
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
+});*/
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/pages/index.html');
 });
 
 server.listen(port, () => {
