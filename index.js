681d8bb21c7156181824c39d574665404c25b16d
fenil292 <65783083+fenil292@users.noreply.github.com>
2022-01-24 17:07:37 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
-/*app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
-});*/
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/pages/messanger.html');
+  res.sendFile(__dirname + '/pages/index.html');
 });
-console.log('listening on *:3000');
-server.listen(port, () => {
-  console.log('listening on *:3000');
+
+io.on('connection', (socket) => {
+  socket.on('chat message', data => {
+    io.emit('message', { name: data.name, message: data.message,time: data.time });
+  });
+});
+/*io.sockets.on( 'connection', function( client ) {
+ 	console.log( "New client !" );
+	
+ 	client.on( 'message', function( data ) {
+ 		console.log( 'Message received ' + data.name + ":" + data.message + ":" + data.time);
+ 		//client.broadcast.emit( 'message', { name: data.name, message: data.message } );
+ 		io.sockets.emit( 'message', { name: data.name, message: data.message,time: data.time } );
+ 	});
+ });*/
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
