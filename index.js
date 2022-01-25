ee2e00e5744fe0fe81c102a6d22c95703ad7ec56
fenil292 <65783083+fenil292@users.noreply.github.com>
2022-01-25 12:54:37 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/pages/messanger.php');
+  res.sendFile(__dirname + '/pages/index.html');
 });
 /*app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
